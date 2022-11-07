f778aabe1579eb743b2db6d9a1318365d19d4bc1
syket-das <saiketsd23@gmail.com>
2022-11-07 17:21:11 +0530

    university basic update prototype

--- src/scenes/universities/universityProfile/UniversityProfileBasic.jsx
+++ src/scenes/universities/universityProfile/UniversityProfileBasic.jsx
@@ -3,14 +3,19 @@ import {
   Box,
   Button,
   CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Divider,
+  TextField,
   Typography,
   useTheme,
 } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import HashLoaderComponent from '../../../components/loader/HashLoaderComponent';
 import TextRow from '../../../components/text/TextRow';
 import { universityProfileBasic } from '../../../redux/actions/university/universityProfileActions';
 import { tokens } from '../../../theme';
@@ -24,19 +29,51 @@ const UniversityProfileBasic = () => {
   const { loading, error, university } = useSelector(
     (state) => state.universityProfile.basic
   );
+  const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     dispatch(universityProfileBasic(params.id));
   }, [dispatch, params.id]);
 
+  const [name, setName] = React.useState('');
+  const [country, setCountry] = React.useState('');
+  const [city, setCity] = React.useState('');
+  const [address, setAddress] = React.useState('');
+  const [website, setWebsite] = React.useState('');
+
+  const handleClickOpen = () => {
+    setOpen(true);
+
+    setName(university?.university?.name);
+    setCountry(university?.university?.country);
+    setCity(university?.university?.city);
+    setAddress(university?.university?.address);
+    setWebsite(university?.university?.website);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleUpdate = () => {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('country', country);
+    formData.append('city', city);
+    formData.append('address', address);
+    formData.append('website', website);
+  };
+
   return (
     <>
       {loading ? (
-        <Box sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <CircularProgress color="success" />
         </Box>
       ) : (
@@ -193,10 +230,122 @@ const UniversityProfileBasic = () => {
               padding: '10px 20px',
               mt: '30px',
             }}
+            onClick={handleClickOpen}
           >
             <Update sx={{ mr: '10px' }} />
             Update Details
           </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            style={
+              {
+                // backgroundColor: colors.primary[400],
+                // opacity: '0.3',
+              }
+            }
+          >
+            <DialogTitle>Update University Basic Details</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To update the basic details of the university, please enter the
+                details here. If you don't want to update click cancel.
+              </DialogContentText>
+              <form>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  label="Name"
+                  type="text"
+                  fullWidth
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="country"
+                  label="Country"
+                  type="text"
+                  fullWidth
+                  value={country}
+                  onChange={(e) => {
+                    setCountry(e.target.value);
+                  }}
+                />
+
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="city"
+                  label="City"
+                  type="text"
+                  fullWidth
+                  value={city}
+                  onChange={(e) => {
+                    setCity(e.target.value);
+                  }}
+                />
+
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="address"
+                  label="Address"
+                  type="text"
+                  fullWidth
+                  value={address}
+                  onChange={(e) => {
+                    setAddress(e.target.value);
+                  }}
+                />
+
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="website"
+                  label="Website"
+                  type="text"
+                  fullWidth
+                  value={website}
+                  onChange={(e) => {
+                    setWebsite(e.target.value);
+                  }}
+                />
+              </form>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                sx={{
+                  backgroundColor: colors.redAccent[700],
+                  color: colors.grey[100],
+                  fontSize: '14px',
+                  fontWeight: 'bold',
+                  padding: '10px 20px',
+                  mt: '30px',
+                }}
+                onClick={handleClose}
+              >
+                Cancel
+              </Button>
+              <Button
+                sx={{
+                  backgroundColor: colors.greenAccent[700],
+                  color: colors.grey[100],
+                  fontSize: '14px',
+                  fontWeight: 'bold',
+                  padding: '10px 20px',
+                  mt: '30px',
+                }}
+                onClick={handleUpdate}
+              >
+                Update
+              </Button>
+            </DialogActions>
+          </Dialog>
         </Box>
       )}
     </>
